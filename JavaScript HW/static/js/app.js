6bd5467609f72c55ad98711e16b1062e54034687
Dipika Kumar <dkumar@bitpay.com>
2019-06-06 21:17:25 -0400

    update

--- JavaScript HW/static/js/app.js
+++ JavaScript HW/static/js/app.js
@@ -1,31 +1,52 @@
 // from data.js
-var tableData = data;
+var table = data;
 
 //Select table body
 // var table = d3.select("tbody")
-
-for (i = 0; i < tableData.length; i ++){
-    var tbody = d3.select("tbody")
-    var row = tbody.append("tr")
-    if('datetime' in data[i]){
-        row.append('td').text(data[i].datetime)
-    }
-    if('city' in data[i]){
-        row.append('td').text(data[i].city)
-    }
-    if('state' in data[i]){
-        row.append('td').text(data[i].state)
-    }
-    if('country' in data[i]){
-        row.append('td').text(data[i].country)
-    }
-    if('shape' in data[i]){
-        row.append('td').text(data[i].shape)
-    }
-    if('durationMinutes' in data[i]){
-        row.append('td').text(data[i].durationMinutes)
-    }
-    if('comments' in data[i]){
-        row.append('td').text(data[i].comments)
+var populateTable = (tableData) => {
+    for (i = 0; i < tableData.length; i++) {
+        //tbody is a tag do nothing. if do . is class. if do # asking for id
+        var tbody = d3.select("tbody")
+        var row = tbody.append("tr")
+        if ('datetime' in tableData[i]) {
+            row.append('td').text(tableData[i].datetime)
+        }
+        if ('city' in tableData[i]) {
+            row.append('td').text(tableData[i].city)
+        }
+        if ('state' in tableData[i]) {
+            row.append('td').text(tableData[i].state)
+        }
+        if ('country' in tableData[i]) {
+            row.append('td').text(tableData[i].country)
+        }
+        if ('shape' in tableData[i]) {
+            row.append('td').text(tableData[i].shape)
+        }
+        if ('durationMinutes' in tableData[i]) {
+            row.append('td').text(tableData[i].durationMinutes)
+        }
+        if ('comments' in tableData[i]) {
+            row.append('td').text(tableData[i].comments)
+        }
     }
 }
+
+populateTable(table);
+
+tableContents = d3.select("tbody");
+
+function clearTable() {
+    tableContents.html("<tr></tr>");
+}
+
+var button = d3.select("#filter-btn")
+button.on('click', () => {
+    d3.event.preventDefault()
+    // var date = d3.select('#datetime').property('value')
+    clearTable();
+    var userInput = d3.select("#datetime");
+    filterDate = userInput.property("value");
+    var filteredTable = table.filter(obj => obj.datetime === filterDate);
+    populateTable(filteredTable);
+})
